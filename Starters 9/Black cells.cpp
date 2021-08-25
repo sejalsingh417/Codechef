56c49ba9efd9902f4e05e96266b0d91383f6530d
sejal <sejalsingh417@gmail.com>
2021-08-25 19:26:00 +0530

    BLACKCEL

---
+++ Starters 9/Black cells.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+
+    int n;
+
+    cin >> n;
+    int box = n * n;
+    int blackcell = box / 2;
+
+    cout << blackcell << endl;
+
+    return 0;
+}
